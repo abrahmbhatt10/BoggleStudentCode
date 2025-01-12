2844484e33e2e82d3b38c8c3cd858c98f44c5816
agastyabrahmbhatt <agastyabrahmbhatt@menloschool.org>
2025-01-12 14:15:34 -0800

    code

--- src/Boggle.java
+++ src/Boggle.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Boggle {
 
@@ -11,11 +12,11 @@ public class Boggle {
             return null;
         }
         //Initialize the dictionary
-        HashMap<String, Integer> mDict = new HashMap();
-        HashMap<String, Integer> mWords = new HashMap();
+        HashSet<String> mDict = new HashSet();
+        HashSet<String> mWords = new HashSet();
         for(int i = 0; i < dictionary.length; i++)
         {
-            mDict.put(dictionary[i], i);
+            mDict.add(dictionary[i]);
         }
         String prefix = "";
         boolean[][] visited = new boolean [board.length][board[0].length];
@@ -44,8 +45,8 @@ public class Boggle {
         Below code converts the hashmap into a sorted array of strings, then return the array.
         Below code taken from: https://stackoverflow.com/questions/1090556/java-how-to-convert-hashmapstring-object-to-array
          */
-        String[] sol = new String[mWords.keySet().size()];
-        mWords.keySet().toArray(sol);
+        String[] sol = new String[mWords.size()];
+        mWords.toArray(sol);
         Arrays.sort(sol);
         return sol;
     }
@@ -61,7 +62,7 @@ public class Boggle {
             mark this square as not visited
 
      */
-    public static void dfs(char[][] grid, int i, int j, boolean[][] visited, String prefix, HashMap<String, Integer> addWords, HashMap<String, Integer> mDict) {
+    public static void dfs(char[][] grid, int i, int j, boolean[][] visited, String prefix, HashSet<String> addWords, HashSet<String> mDict) {
         if (i < 0 || j < 0 || i >= grid.length
                 || j >= grid[0].length)  return;
 
@@ -69,9 +70,9 @@ public class Boggle {
 
         String currentPrefix = prefix + grid[i][j];
         // Below if statement checks if the word is in dictionary and isn't already in addWords.
-        if((mDict.get(currentPrefix) != null) && (addWords.get(currentPrefix) == null)) {
+        if(mDict.contains(currentPrefix) && !addWords.contains(currentPrefix)) {
             //Valid prefix
-            addWords.put(currentPrefix, addWords.size());
+            addWords.add(currentPrefix);
         }
         // Mark this square as visited
         visited[i][j] = true;
