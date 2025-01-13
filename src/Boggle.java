50e15622f63663e5410e20027faa28e6b84fb2de
agastyabrahmbhatt <agastyabrahmbhatt@menloschool.org>
2025-01-12 18:29:02 -0800

    code

--- src/Boggle.java
+++ src/Boggle.java
@@ -11,11 +11,13 @@ public class Boggle {
             return null;
         }
         //Initialize the dictionary
-        HashSet mDict = new HashSet();
-        HashSet mWords = new HashSet();
+        //HashSet mDict = new HashSet();
+        //HashSet mWords = new HashSet();
+        TST mDict = new TST();
+        HashSet<String> mWords = new HashSet<String>();
         for(int i = 0; i < dictionary.length; i++)
         {
-            mDict.add(dictionary[i]);
+            mDict.put(dictionary[i], i);
         }
         String prefix = "";
         boolean[][] visited = new boolean [board.length][board[0].length];
@@ -44,9 +46,13 @@ public class Boggle {
             Below code converts the hashmap into a sorted array of strings, then return the array.
             Below code taken from: https://stackoverflow.com/questions/1090556/java-how-to-convert-hashmapstring-object-to-array
         */
-        ArrayList<String> goodWords = new ArrayList<String>(mWords);
-        String[] sol = new String[goodWords.size()];
-        goodWords.toArray(sol);
+        //ArrayList<String> goodWords = new ArrayList<String>(mWords);
+        //String[] sol = new String[goodWords.size()];
+        //for(String word: mWords) {
+          //  System.out.println(word);
+        //}
+        String[] sol = new String[mWords.size()];
+        mWords.toArray(sol);
         Arrays.sort(sol);
         return sol;
     }
@@ -62,26 +68,39 @@ public class Boggle {
             mark this square as not visited
 
      */
-    public static void dfs(char[][] grid, int i, int j, boolean[][] visited, String prefix, HashSet addWords, HashSet mDict) {
+    public static void dfs(char[][] grid, int i, int j, boolean[][] visited, String prefix, HashSet<String> addWords, TST mDict) {
         if (i < 0 || j < 0 || i >= grid.length
                 || j >= grid[0].length)  return;
 
         if (visited[i][j]) return;
 
         String currentPrefix = prefix + grid[i][j];
+        int subMatch = 0;
         // Below if statement checks if the word is in dictionary and isn't already in addWords.
-        if(mDict.contains(currentPrefix)) {
-            //Valid prefix
+        if(mDict.get(currentPrefix) != -1) {
+            //Invalid prefix
             addWords.add(currentPrefix);
+        } else {
+            subMatch = mDict.getSubMatch();
+            if(subMatch != currentPrefix.length()-1)
+            {
+                return;
+            }
         }
-        // Mark this square as visited
         visited[i][j] = true;
-        dfs(grid, i - 1, j, visited, currentPrefix,addWords, mDict);
-        dfs(grid, i + 1, j, visited, currentPrefix,addWords, mDict);
-        dfs(grid, i, j - 1, visited, currentPrefix,addWords, mDict);
-        dfs(grid, i, j + 1, visited, currentPrefix,addWords, mDict);
-
-        // Mark this square as unvisited
+        // Mark this square as visited
+        if(i-1 >= 0 && j >= 0 && i-1 < grid.length && j <grid[0].length && (!visited[i-1][j])){
+            dfs(grid, i - 1, j, visited, currentPrefix,addWords, mDict);
+        }
+        if(i+1 >= 0 && j >= 0 && i+1 < grid.length && j <grid[0].length &&(!visited[i+1][j])) {
+            dfs(grid, i + 1, j, visited, currentPrefix,addWords, mDict);
+        }
+        if(i >= 0 && j-1 >= 0 && i < grid.length && j-1 <grid[0].length &&(!visited[i][j-1])) {
+            dfs(grid, i, j - 1, visited, currentPrefix,addWords, mDict);
+        }
+        if(i >= 0 && j+1 >= 0 && i < grid.length && j+1 <grid[0].length &&(!visited[i][j+1])){
+            dfs(grid, i, j + 1, visited, currentPrefix,addWords, mDict);
+        }
         visited[i][j] = false;
     }
 }
--- src/TST.java
+++ src/TST.java
@@ -1,64 +1,67 @@
+
 /*
 Below class taken from Algorithms 4th Edition by Roger Sedgewick and Kevin Wayne
  */
 public class TST {
     private Node root;
+    private int subMatch = 0;
 
     private class Node {
         private char c;
-        private Node left, mid, right;
-        private int val;
+        private Node left=null, mid=null, right=null;
+        private int val=-1;
     }
 
     public int get(String key){
+        subMatch = 0;
         Node x = get(root, key, 0);
         if (x == null){
             return -1;
         }
         return x.val;
     }
-
-    private Node get(Node x, String key, int d){
-        if (x == null){
+    public  int getSubMatch(){
+        return subMatch;
+    }
+    private Node get(Node x, String key, int d) {
+        if (x == null) {
             return null;
         }
         char c = key.charAt(d);
-        if (c < x.c){
+        if (c < x.c) {
             return get(x.left, key, d);
-        }
-        else if (c > x.c){
+        } else if (c > x.c) {
             return get(x.right, key, d);
-        }
-        else if (d < key.length() - 1){
-            return get(x.mid, key, d+1);
-        }
-        else{
+        } else if (d < key.length() - 1) {
+            subMatch++;
+            return get(x.mid, key, d + 1);
+        } else {
             return x;
         }
+    }
 
-        public void put(String key, int val){
+    public void put(String key, int val){
             root = put(root, key, val, 0);
-        }
+    }
 
-        private Node put(Node x, String key, int val, int d){
+    private Node put(Node x, String key, int val, int d){
             char c = key.charAt(d);
             if (x == null){
                 x = new Node();
                 x.c = c;
             }
             if (c < x.c){
-                x.left = put(x.left, key, d);
+                x.left = put(x.left, key, val, d);
             }
             else if (c > x.c){
-                x.right = put(x.right, key, d);
+                x.right = put(x.right, key, val, d);
             }
             else if (d < key.length() - 1){
-                x.mid = put(x.mid, key, d+1);
+                x.mid = put(x.mid, key, val, d+1);
             }
             else{
                 x.val = val;
             }
-        }
-        return x;
+            return x;
     }
 }
